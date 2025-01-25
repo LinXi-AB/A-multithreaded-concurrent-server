c4f2a423d85fc04ad72f333ec6d9748b0a452ad2
LinXi-AB <784410173@qq.com>
2025-01-25 18:58:55 +0800

    Add project files.

---
+++ client.cpp
@@ -0,0 +1,128 @@
+﻿#include <arpa/inet.h>   // 提供 sockaddr_in、inet_pton() 等网络相关功能
+#include <cstring>      
+#include <iostream>     
+#include <sys/socket.h>  // 提供 socket()、connect()、send() 和 recv() 等函数
+#include <unistd.h>      // 提供 close() 函数
+
+class TCPClient
+{
+private:
+	int clientSocket;
+	struct sockaddr_in serverAddr;
+
+public:
+	TCPClient();
+	~TCPClient();
+
+	bool connectToServer(const std::string& ipAddress, int port);
+	bool sendMessage(const std::string& message);
+	std::string receiveMessage();
+	void closeConnection();
+};
+
+TCPClient::TCPClient() : clientSocket(-1)
+{
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET; // 使用 IPv4
+}
+
+TCPClient::~TCPClient()
+{
+	closeConnection();
+}
+
+// 连接到服务器
+bool TCPClient::connectToServer(const std::string& ipAddress, int port)
+{
+	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == -1)
+	{
+		perror("Socket creation failed");
+		return false;
+	}
+
+	serverAddr.sin_port = htons(port); // 设置端口
+	if (inet_pton(AF_INET, ipAddress.c_str(), &serverAddr.sin_addr) <= 0)
+	{
+		perror("Invalid address or address not supported");
+		return false;
+	}
+
+	// 连接服务器
+	if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
+	{
+		perror("Connection to server failed, 请先启动服务端");
+		return false;
+	}
+	return true;
+}
+
+// 发送消息
+bool TCPClient::sendMessage(const std::string& message)
+{
+	if (send(clientSocket, message.c_str(), message.size(), 0) < 0)
+	{
+		perror("Failed to send message");
+		return false;
+	}
+	return true;
+}
+
+// 接收消息
+std::string TCPClient::receiveMessage()
+{
+	char buffer[1024] = { 0 }; // 定义缓冲区存储接收的数据
+	ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+	if (bytesRead < 0)
+	{
+		perror("Failed to receive message");
+		return "";
+	}
+
+	buffer[bytesRead] = '\0'; // 确保字符串以 '\0' 结尾
+	return std::string(buffer);
+}
+
+// 关闭连接
+void TCPClient::closeConnection()
+{
+	if (clientSocket >= 0)
+	{
+		close(clientSocket);
+		clientSocket = -1;
+	}
+}
+
+/**
+ * @brief 使用流程：
+ * connectToServer(IP, 端口): socket() connect()
+ * sendMessage(字符串)
+ * reveiveMessage()
+ * closeConnection()
+ */
+int main()
+{
+	TCPClient client;
+
+	std::string ip_addr("172.30.129.33");
+	const int port = 8080;
+	if (!client.connectToServer(ip_addr, port))// 测试的时候可以使用127.0.0.1
+	{
+		return -1;
+	}
+
+	if (!client.sendMessage("\"我是客户端\""))
+	{
+		return -1;
+	}
+
+	std::string response = client.receiveMessage();
+	if (!response.empty())
+	{
+		std::cout << "接收到来自服务器 (IP: " << ip_addr
+			<< ", 端口: " << port << ") 的数据: " << response << std::endl;
+	}
+
+	client.closeConnection();
+	return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,229 @@
+﻿#include <arpa/inet.h> // 提供 sockaddr_in、htons() 等网络相关功能
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <string.h>
+#include <thread>
+#include <unistd.h>
+#include <vector>
+
+#define BUFFER_SIZE 1024
+
+class ThreadPool
+{
+public:
+	ThreadPool(size_t thread_count);
+	~ThreadPool();
+	void addTask(const std::function<void()>& task);
+
+private:
+	void worker();
+
+	std::vector<std::thread> threads;
+	std::queue<std::function<void()>> tasks_queue;
+	std::mutex queue_mutex;
+	std::condition_variable condition;
+	std::atomic<bool> stop = false;
+};
+
+ThreadPool::ThreadPool(size_t thread_count)
+{
+	for (size_t i = 0; i < thread_count; ++i)
+	{
+		threads.emplace_back(&ThreadPool::worker, this);
+	}
+}
+
+ThreadPool::~ThreadPool()
+{
+	stop.store(true);
+	condition.notify_all();
+	for (auto& thread : threads)
+	{
+		if (thread.joinable())
+			thread.join();
+	}
+}
+
+void ThreadPool::addTask(const std::function<void()>& task)
+{
+	{
+		std::lock_guard<std::mutex> lock(queue_mutex);
+		tasks_queue.push(task);
+	}
+	condition.notify_one();
+}
+
+void ThreadPool::worker()
+{
+	while (true)
+	{
+		std::function<void()> task;
+		{
+			std::unique_lock<std::mutex> lock(queue_mutex);
+			condition.wait(lock, [this]
+				{
+					return !tasks_queue.empty() || stop;
+				});
+
+			if (stop && tasks_queue.empty())
+				return;
+
+			task = std::move(tasks_queue.front());
+			tasks_queue.pop();
+		}
+		task();
+	}
+}
+
+class TCPServer
+{
+private:
+	int server_fd;
+	struct sockaddr_in address;
+	int addrlen;
+
+public:
+	TCPServer(int port);
+	~TCPServer();
+	bool initialize();
+	int acceptClient();
+	void closeServer();
+};
+
+TCPServer::TCPServer(int port) : server_fd(-1), addrlen(sizeof(address))
+{
+	memset(&address, 0, sizeof(address));
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(port);
+}
+
+TCPServer::~TCPServer()
+{
+	closeServer();
+}
+
+bool TCPServer::initialize()
+{
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == 0)
+	{
+		perror("Socket creation failed");
+		return false;
+	}
+
+	int opt = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) != 0)
+	{
+		perror("Setsockopt failed");
+		close(server_fd);
+		return false;
+	}
+
+	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
+	{
+		perror("Bind failed");
+		close(server_fd);
+		return false;
+	}
+
+	if (listen(server_fd, 3) < 0)
+	{
+		perror("Listen failed");
+		close(server_fd);
+		return false;
+	}
+	return true;
+}
+
+int TCPServer::acceptClient()
+{
+	int client_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+	if (client_socket < 0)
+	{
+		perror("Accept failed");
+		return -1;
+	}
+	return client_socket;
+}
+
+void TCPServer::closeServer()
+{
+	if (server_fd >= 0)
+	{
+		close(server_fd);
+		server_fd = -1;
+	}
+}
+
+void handleClient(int client_socket)
+{
+	static std::atomic<int> message_counter(1);  // 消息编号计数器
+
+	char buffer[BUFFER_SIZE] = { 0 };
+	int bytesRead = read(client_socket, buffer, BUFFER_SIZE);
+
+	if (bytesRead > 0)
+	{
+		// 获取消息编号
+		int message_id = message_counter++;
+
+		// 输出接收到的原始数据和消息编号
+		std::cout << "(#" << message_id << ") Received message from client: " << std::string(buffer, bytesRead) << std::endl;
+
+		// 输出客户端的 IP 地址和端口
+		struct sockaddr_in client_addr;
+		socklen_t client_len = sizeof(client_addr);
+		getpeername(client_socket, (struct sockaddr*)&client_addr, &client_len);
+		std::cout << "Client IP: " << inet_ntoa(client_addr.sin_addr) << std::endl;
+		std::cout << "Client port: " << ntohs(client_addr.sin_port) << std::endl;
+
+		// 返回给客户端的数据，包含消息编号
+		std::string response = "Message #" + std::to_string(message_id) + " - Server response: " + std::string(buffer, bytesRead);
+		send(client_socket, response.c_str(), response.size(), 0);
+
+		// 输出服务器响应内容
+		std::cout << "Sent response to client: " << response << std::endl;
+	}
+	else
+	{
+		std::cerr << "Failed to read data from client" << std::endl;
+	}
+
+	close(client_socket);
+}
+
+
+int main()
+{
+	const int port = 8080;
+	TCPServer server(port);
+
+	if (!server.initialize())
+	{
+		return -1;
+	}
+
+	ThreadPool pool(4);
+
+	std::cout << "Server is running on port " << port << "..." << std::endl;
+
+	while (true)// 服务器主循环
+	{
+		int client_socket = server.acceptClient();// 阻塞等待一个客户端的链接
+		if (client_socket >= 0)// 连接成功则分发任务给线程
+		{
+			pool.addTask([client_socket]
+				{
+					handleClient(client_socket);
+				});
+		}
+	}
+
+	server.closeServer();
+	return 0;
+}
